4d6c8ce8c7109aa4137c42b19ac5521f0c4d1d43
Scott Recker <scott.a.recker@gmail.com>
2024-06-06 14:38:14 -0500

    Add Xcode project

---
+++ Recipe Browser/Recipe_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_BrowserApp.swift
+//  Recipe Browser
+//
+//  Created by Scott Recker on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
